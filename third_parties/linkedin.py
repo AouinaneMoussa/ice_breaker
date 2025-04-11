9ed8361a49a06c635e8b068ac48b42d5322b07be
AouinaneMoussa <moussa.aouinane@gmail.com>
2025-04-11 15:13:52 +0200

    linkedin-scraping-last-version

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -11,29 +11,33 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     Mannually scrape the information from the LinkedIn profile"""
 
     if mock:
-        linkedin_profile_url = "https://gist.githubusercontent.com/AouinaneMoussa/a62768bfdfa5fb74db86e790b4234559/raw/2428616fd8eb06e502898777253e04fafafbde94/moussa-aouinane-scrapin.json"
+        linkedin_profile_url = "https://gist.githubusercontent.com/AouinaneMoussa/f80909ea4d5b14be411fbea8ead77f57/raw/920a3627318d722a79efaadc8c0cee2d0c93669a/moussa-aouinane.json"
         response = requests.get(
             linkedin_profile_url, 
             timeout=10,
         )
     else:
-        api_endpoint = "https://api.scrapin.io/enrichment/profile"
-        params = {
-            "apikey": os.environ["SCRAPIN_API_KEY"],
-            "linkedInUrl": linkedin_profile_url,
-        }
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        header = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
         response = requests.get(
             api_endpoint,
-            params=params,
+            params={
+            "url": linkedin_profile_url
+            },
+            headers=header,
             timeout=10,
         )
 
-    data = response.json().get("person")
+    data = response.json()
     data = {
         k: v 
         for k, v in data.items()
-        if v not in ([], "", "", None) and k not in ["certifications"]
+        if v not in ([], "", "", None) and k not in ["people_also_viewed","certifications"]
     }
+
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
     
     return data
 
